e94d54a16244d1d910ede7b0a2774fefa5cd92b2
Lenin Chura <lenin1alonso@gmail.com>
2024-09-27 08:00:04 -0500

    Controller Permisos y roles

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
-/* IMPORTAMOS LAS LIBRERIAS */
+/* IMPORTAMOS DE DOTENV */
 import dotenv from 'dotenv';
 dotenv.config()
+
+/** IMPORTAMOS DE LIBRERIAS  */
 import express from 'express';
+const cors = require('cors');
+
+/** IMPORTAMOS RUTAS DE ARCHIVOS */
 import authRoutes from './routes/AuthRoutes'
+import roleRoutes from './routes/RoleRoutes'
+import permission from './routes/PermissionRoutes'
 
 // APP CON EXPRESS
 const app = express()
@@ -11,7 +18,14 @@ app.use(express.json());
 
 // RUTAS
 app.use('/auth', authRoutes);
+app.use('/api', roleRoutes);
+app.use('/api', permission)
+
 // AUTENTICACION
+
+
+// CORS
+app.use(cors());
 
 // Hacer una apirest de usuarios
 
